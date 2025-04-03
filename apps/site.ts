6496eac1b34c8b6e23419abd1d360f0927855d47
soutofernando <fernandosoutol45@gmail.com>
2025-04-03 20:25:59 -0300

    chore: clear rep

--- apps/site.ts
+++ apps/site.ts
@@ -1,25 +1,30 @@
-import blog from "apps/blog/mod.ts";
-import website, { Props } from "apps/website/mod.ts";
+import website, { Props as WebsiteProps } from "apps/website/mod.ts";
 import manifest, { Manifest } from "../manifest.gen.ts";
 import { type App as App, type AppContext as AC } from "@deco/deco";
+import { Secret } from "apps/website/loaders/secret.ts";
+
 type WebsiteApp = ReturnType<typeof website>;
-type BlogApp = ReturnType<typeof blog>;
+
+interface Props extends WebsiteProps {
+    figmaAccessToken: Secret;
+}
+type PropsWithKeys = Omit<Props, "figmaAccessToken"> & {
+    figmaAccessToken: string;
+};
 /**
  * @title Site
  * @description Start your site from a template or from scratch.
  * @category Tool
  * @logo https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1/0ac02239-61e6-4289-8a36-e78c0975bcc8
  */
-export default function Site(state: Props): App<Manifest, Props, [
+export default function Site(state: Props): App<Manifest, PropsWithKeys, [
   WebsiteApp,
-  BlogApp,
 ]> {
   return {
-    state,
+    state: { ...state, figmaAccessToken: state.figmaAccessToken?.get() ?? "" },
     manifest,
     dependencies: [
       website(state),
-      blog(state),
     ],
   };
 }
