50650d3cf81dd6a3b99f601c0bb5bca8f9f50a2b
soutofernando <fernandosoutol45@gmail.com>
2025-04-03 20:26:23 -0300

    feat: get Component

---
+++ loaders/getComponent.ts
@@ -0,0 +1,37 @@
+import { AppContext } from "site/apps/site.ts";
+
+export default async function loader(
+    props: { key: string; nodeId: string },
+    _req: Request,
+    ctx: AppContext,
+) {
+
+    const figmaAccessToken = ctx.figmaAccessToken;
+
+    if (!props.key || !props.nodeId) {
+        throw new Error("key e nodeId são obrigatórios");
+    }
+
+    if (!figmaAccessToken) {
+        throw new Error("figmaAccessToken não configurado");
+    }
+
+    const response = await fetch(
+        `https://api.figma.com/v1/files/${props.key}/nodes?ids=${props.nodeId}`,
+        {
+            headers: {
+                "X-FIGMA-TOKEN": figmaAccessToken,
+            },
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(`Erro ao buscar componente: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export const cache = "no-cache";
+
